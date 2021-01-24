d5448bccc93b6cba4120548fc0c4f8674813d27a
Mark Pim <j.mark.pim@gmail.com>
2021-01-24 14:47:41 +0000

    WIP

---
+++ src/vk.registry.reader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using vk.registry.model;
+
+namespace vk.registry.reader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string RegistryUrl = @"https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/master/registry/vk.xml";
+
+            string registryXml;
+
+            using (var client = new HttpClient())
+            {
+                registryXml = await client.GetStringAsync(RegistryUrl);
+            }
+
+            var registry = Registry.ReadFrom(registryXml);
+        }
+    }
+}
